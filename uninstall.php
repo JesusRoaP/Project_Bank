bd71a05d3500599123298b9c62b13a16d1553879
Administración <administrador@iMac-de-Administracion.local>
2020-11-09 15:27:20 -0500

    Se agregaron las instrucciones de borrar tabla wp_proyectos al desinstalar el plugin

--- uninstall.php
+++ uninstall.php
@@ -1,8 +1,13 @@
 <?php
-if (!defined('WP_UNINSTALL_PLUGIN')) {
-    die;
-}
-
 //Instrucciones para desintalar el plugin Banco de Proyectos
+
+if (!defined('ABSPATH') && !defined('WP_UNINSTALL_PLUGIN')) {
+    exit();
+}
+// Borrar tabla wp_proyectos
+global $wpdb;
+$table_name = $wpdb->prefix . 'proyectos';
+
+$wpdb->query($wpdb->prepare( "DROP TABLE $table_name" ));
 
 ?>
