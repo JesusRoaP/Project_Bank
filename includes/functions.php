664610a8ff7fade3d79911574de1fa40f8961718
Administración <administrador@iMac-de-Administracion.local>
2020-11-12 15:02:55 -0500

    Ajuste de titulo pagina proyecto individual

--- includes/functions.php
+++ includes/functions.php
@@ -166,29 +166,28 @@ function proyecto() {
 }
 add_shortcode('project', 'proyecto');
 
-function filter_project_title_content($title) {
-    $codigo = $_GET['codigo'];
-    if(  is_page('Proyecto') ) {
-        $title = "Proyecto " . $codigo;
-    }
-    return $title;
+function filter_project_title_content( $titulo ) {
+	$codigo = $_GET['codigo'];
+	if ( is_page('Proyecto codigo') ) {
+		$titulo = str_replace('codigo', $codigo, $titulo);
+	}
+    return $titulo;
 }
-add_filter('the_title', 'filter_project_title_content');
+add_filter( 'the_title', 'filter_project_title_content' );
 
 function filter_project_wpseo_title($title) {
-    if(  is_page('Proyecto') ) {
+    if( is_page('Proyecto codigo') ) {
         $title = get_the_title() . " - Banco de Proyectos - " . get_bloginfo( 'name' );
     }
     return $title;
 }
 add_filter('wpseo_title', 'filter_project_wpseo_title');
 
-function filter_project_title() {
-	$codigo = $_GET['codigo'];
-	if (isset($codigo)) {
-		$project_page_name = get_the_title() . " " . "$codigo" . " - Banco de Proyectos - " . get_bloginfo( 'name' ); 
+function filter_project_title($title) {
+	if ( is_page('Proyecto codigo') ) {
+		$title = get_the_title() . " - Banco de Proyectos - " . get_bloginfo( 'name' ); 
 	}
-	return $project_page_name;
+	return $title;
 }
 add_filter('pre_get_document_title', 'filter_project_title');
 
