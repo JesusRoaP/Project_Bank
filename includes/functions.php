cb7b254b2a0413879aeba1a24f05a94376d4fe3c
Administración <administrador@iMac-de-Administracion.local>
2020-11-12 17:39:57 -0500

    Se agrega cmabio de titulo de pagina individual de proyectos para rank math seo

--- includes/functions.php
+++ includes/functions.php
@@ -175,13 +175,21 @@ function filter_project_title_content( $titulo ) {
 }
 add_filter( 'the_title', 'filter_project_title_content' );
 
-function filter_project_wpseo_title($title) {
+function filter_project_wpyoastseo_title($title) {
     if( is_page('Proyecto codigo') ) {
         $title = get_the_title() . " - Banco de Proyectos - " . get_bloginfo( 'name' );
     }
     return $title;
 }
-add_filter('wpseo_title', 'filter_project_wpseo_title');
+add_filter('wpseo_title', 'filter_project_wpyoastseo_title');
+
+function filter_project_wprankmathseo_title( $title ) {
+    if( is_page('Proyecto codigo') ) {
+        $title = get_the_title() . " - Banco de Proyectos - " . get_bloginfo( 'name' );
+    }
+    return $title;
+}
+add_filter( 'rank_math/frontend/title', 'filter_project_wprankmathseo_title');
 
 function filter_project_title($title) {
 	if ( is_page('Proyecto codigo') ) {
