994d8b6d1c29aebc5cb0ec6e3f681186cbee58a9
Administración <administrador@iMac-de-Administracion.local>
2020-11-17 11:52:06 -0500

    Actualizacion de permisos para el perfil o rol admin_pb

--- includes/functions.php
+++ includes/functions.php
@@ -295,18 +295,23 @@ function admin_banco_proyectos() {
 }
 add_shortcode('project_bank_admin', 'admin_banco_proyectos');
 
-//Restringir panel admin solo a administradores
+//Restringir panel admin al perfil bp_admin
 function restrict_admin_area_by_rol() {
     if (!current_user_can('manage_options') && (!defined('DOING_AJAX') || ! DOING_AJAX )) {
-        wp_redirect(site_url());
-        exit;
-    }
+		$user = wp_get_current_user();
+    	if( ! empty( $user ) && in_array( "admin_pb", (array) $user->roles ) ) {
+			wp_redirect(site_url());
+    		exit;
+		}
+	}
 }
 add_action('admin_init', 'restrict_admin_area_by_rol', 1);
 
-// Mostrar barra admin solo a administradores
+// Mostrar barra admin al perfil bp_admin
 function hide_admin_bar($content) {
-    return ( current_user_can('manage_options') ) ? $content : false;
+	$user = wp_get_current_user();
+	if( ! empty( $user ) && in_array( "admin_pb", (array) $user->roles )) $content = false;
+		return $content;
 }
 add_filter( 'show_admin_bar' , 'hide_admin_bar');
 ?>
