6490943f73121cb8c0720cba84c2a03dd3d97d3b
unknown <mauricio.roa@outllook.com>
2021-01-19 16:44:12 -0500

    Actualizacion Info Plugin

--- admin/configuration.php
+++ admin/configuration.php
@@ -8,7 +8,7 @@ if (! current_user_can ('manage_options')) wp_die (__ ('No tienes suficientes pe
 	Bienvenido a la configuración del plugin Project Bank
 </div>
 <br>
-<div class="pb-info" style="width: 90%">
+<div class="pb-info">
 	<ul>
 		<li>Utiliza el siguiente shortcode para mostrar el Banco de Proyectos (Página principal): <input id="shortcode-1" type="text" value="[project_bank]"><button class="button" onclick="copyToClipboard('#shortcode-1')">Copiar Shortcode</button><span class="copy" style="display: none">Copiado!</span></li>
 		<li>Utiliza el siguiente shortcode para mostrar los detalles de cada proyecto en una página individual: <input id="shortcode-2" type="text" value="[project]"><button class="button" onclick="copyToClipboard('#shortcode-2')">Copiar Shortcode</button><span class="copy" style="display: none">Copiado!</span>
